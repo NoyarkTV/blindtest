1ae160a0b3f5b7d20a21b2dd12cc9b6304da2fca
NoyarkTV <68473677+NoyarkTV@users.noreply.github.com>
2025-05-26 13:31:39 +0000

    correction landing page redirect

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,7 +5,7 @@ function LandingPage() {
     <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>🎧 Bienvenue sur Blindtest</h1>
       <p>Connecte-toi à Spotify pour démarrer une partie !</p>
-      <a href="http://localhost:8888/login">
+      <a href="/login">
         <button style={{ padding: "10px 30px", fontSize: "18px" }}>
           Se connecter à Spotify
         </button>
