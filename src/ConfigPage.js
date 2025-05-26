c81f81be56aa49983875890bb1a48707ec24ff20
Thibault Choffardet <mathibault77@gmail.com>
2025-05-26 21:12:28 +0200

    modif package

--- src/ConfigPage.js
+++ src/ConfigPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+console.log("✅ ConfigPage.js chargé !");
+
 function ConfigPage() {
   const navigate = useNavigate();
   const [time, setTime] = useState(30);
