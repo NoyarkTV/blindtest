aa6db8a2407890cf841954922eb34886550f72b8
Thibault Choffardet <mathibault77@gmail.com>
2025-05-26 21:23:22 +0200

    fix now working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import GamePage from "./GamePage";
 import ConfigPage from "./ConfigPage";
