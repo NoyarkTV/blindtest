f0f63c6a7aa7a7402b1e10df21ca52886e6cb425
Thibault Choffardet <mathibault77@gmail.com>
2025-06-02 18:28:10 +0200

    Reboot gamepage 3

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
-const cors = require("cors");
 const open = (...args) => import('open').then(mod => mod.default(...args));
 let storedAccessToken = null;
 let storedRefreshToken = null;
 let ready = false;
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 
