563b4d9cc4b23e6279da4b1ab6f54ee40259d60c
Thibault Choffardet <mathibault77@gmail.com>
2025-06-03 17:53:24 +0200

    add style

--- src/GamePage.js
+++ src/GamePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import SpotifyPlayer from "./SpotifyPlayer";
 import socket from "./socket";
