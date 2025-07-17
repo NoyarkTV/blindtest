be3fe8987f6918671f4b1bb8d72240413636dfe8
NoyarkTV <68473677+NoyarkTV@users.noreply.github.com>
2025-07-17 10:58:33 +0000

    landing page overall and app.css upgraded

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import './App.css';
 
 function LandingPage({ isSpotifyConnected, onConnectSpotify }) {
   const navigate = useNavigate();
